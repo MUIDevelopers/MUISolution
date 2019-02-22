becaba92f512e53e5a90771a62240e47211b0373
mochida <mike_ochida@yahoo.com>
2019-02-22 16:22:58 +0800

    Added New Button 3

--- MUISolution/Form1.Designer.vb
+++ MUISolution/Form1.Designer.vb
@@ -24,6 +24,8 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
         Me.DirectorySearcher1 = New System.DirectoryServices.DirectorySearcher()
+        Me.SimpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton3 = New DevExpress.XtraEditors.SimpleButton()
         Me.SuspendLayout()
         '
         'SimpleButton1
@@ -32,7 +34,7 @@ Partial Class Form1
         Me.SimpleButton1.Name = "SimpleButton1"
         Me.SimpleButton1.Size = New System.Drawing.Size(75, 23)
         Me.SimpleButton1.TabIndex = 0
-        Me.SimpleButton1.Text = "Test Button 1"
+        Me.SimpleButton1.Text = "Nice Button"
         '
         'DirectorySearcher1
         '
@@ -40,11 +42,29 @@ Partial Class Form1
         Me.DirectorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01")
         Me.DirectorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01")
         '
+        'SimpleButton2
+        '
+        Me.SimpleButton2.Location = New System.Drawing.Point(223, 113)
+        Me.SimpleButton2.Name = "SimpleButton2"
+        Me.SimpleButton2.Size = New System.Drawing.Size(75, 23)
+        Me.SimpleButton2.TabIndex = 1
+        Me.SimpleButton2.Text = "Nice Button2"
+        '
+        'SimpleButton3
+        '
+        Me.SimpleButton3.Location = New System.Drawing.Point(385, 84)
+        Me.SimpleButton3.Name = "SimpleButton3"
+        Me.SimpleButton3.Size = New System.Drawing.Size(75, 23)
+        Me.SimpleButton3.TabIndex = 2
+        Me.SimpleButton3.Text = "Nice Button2"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SimpleButton3)
+        Me.Controls.Add(Me.SimpleButton2)
         Me.Controls.Add(Me.SimpleButton1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -54,4 +74,6 @@ Partial Class Form1
 
     Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents DirectorySearcher1 As DirectoryServices.DirectorySearcher
+    Friend WithEvents SimpleButton2 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton3 As DevExpress.XtraEditors.SimpleButton
 End Class
--- MUISolution/Form1.vb
+++ MUISolution/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
         MsgBox("Hello World 123")
     End Sub
+
+    Private Sub SimpleButton2_Click(sender As Object, e As EventArgs) Handles SimpleButton2.Click
+        MsgBox("New Button")
+    End Sub
 End Class
