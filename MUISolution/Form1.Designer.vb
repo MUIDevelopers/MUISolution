a595c40ae14fbf8bfe182e66a5b5eea0b20cfae3
mochida <mochida@moylans.net>
2019-02-22 16:48:25 +0800

    Remove button 3 no code

--- MUISolution/Form1.Designer.vb
+++ MUISolution/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,11 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
         Me.DirectorySearcher1 = New System.DirectoryServices.DirectorySearcher()
         Me.SimpleButton2 = New DevExpress.XtraEditors.SimpleButton()
-        Me.SimpleButton3 = New DevExpress.XtraEditors.SimpleButton()
         Me.SuspendLayout()
         '
         'SimpleButton1
@@ -50,20 +49,11 @@ Partial Class Form1
         Me.SimpleButton2.TabIndex = 1
         Me.SimpleButton2.Text = "Nice Button2"
         '
-        'SimpleButton3
-        '
-        Me.SimpleButton3.Location = New System.Drawing.Point(385, 84)
-        Me.SimpleButton3.Name = "SimpleButton3"
-        Me.SimpleButton3.Size = New System.Drawing.Size(75, 23)
-        Me.SimpleButton3.TabIndex = 2
-        Me.SimpleButton3.Text = "Nice Button2"
-        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.SimpleButton3)
         Me.Controls.Add(Me.SimpleButton2)
         Me.Controls.Add(Me.SimpleButton1)
         Me.Name = "Form1"
@@ -75,5 +65,4 @@ Partial Class Form1
     Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents DirectorySearcher1 As DirectoryServices.DirectorySearcher
     Friend WithEvents SimpleButton2 As DevExpress.XtraEditors.SimpleButton
-    Friend WithEvents SimpleButton3 As DevExpress.XtraEditors.SimpleButton
 End Class
